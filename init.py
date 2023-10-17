3caa209d04495b87b7ad838fafba6413d2a8b555
jfabi <jacopo.fabi8@gmail.com>
2023-10-17 13:06:27 +0200

    rimozione init

--- init.py
+++
@@ -1,8 +0,0 @@
-import os
-
-current_folder = os.getpwd()
-os.makedirs(os.path.join(current_folder, r"resrcs\datasets\textAwareMultiGan"), exist_ok=True)
-os.makedirs(os.path.join(current_folder, r"resrcs\datasets\textDetection"), exist_ok=True)
-os.makedirs(os.path.join(current_folder, r"resrcs\neuralNetworks\textAwareMultiGan"), exist_ok=True)
-os.makedirs(os.path.join(current_folder, r"resrcs\neuralNetworks\textDetection"), exist_ok=True)
-os.makedirs(os.path.join(current_folder, r"resrcs\results"), exist_ok=True)
